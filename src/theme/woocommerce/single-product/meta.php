09d0cb76495b220c4c0d19446d965e17a9f2f2c1
Luan Gjokaj <luan@deep-impact.ch>
2017-10-04 22:49:36 +0200

    Tag style consistency.

--- src/theme/woocommerce/single-product/meta.php
+++ src/theme/woocommerce/single-product/meta.php
@@ -34,7 +34,7 @@ global $product;
 
 	<?php echo wc_get_product_category_list( $product->get_id(), ', ', '<span class="posted_in">' . _n( 'Category:', 'Categories:', count( $product->get_category_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?>
 
-	<?php echo wc_get_product_tag_list( $product->get_id(), ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', count( $product->get_tag_ids() ), 'woocommerce' ) . ' ', '</span>' ); ?>
+	<?php echo wc_get_product_tag_list( $product->get_id(), '', '<span class="tagged_as">' . _n( 'Tag:', '', count( $product->get_tag_ids() ), 'woocommerce' ), '</span>' ); ?>
 
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
 
