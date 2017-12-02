7154b6009ecff807a6e9163cfdc2cd33d2ef99cc
Luan Gjokaj <luan@deep-impact.ch>
2017-12-02 17:57:52 +0100

    Hardcoded favicon.

--- src/theme/header.php
+++ src/theme/header.php
@@ -4,6 +4,7 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width">
 	<?php wp_head(); ?>
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/img/favicon.ico" type="image/x-icon">
 </head>
 
 <body <?php body_class(); ?>>
