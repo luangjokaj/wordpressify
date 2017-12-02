6434472f266646204f767efb36da955c7c70f9ee
Luan Gjokaj <luan@deep-impact.ch>
2017-12-02 16:35:42 +0100

    Show all categories posts. Cleanup.

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -1,8 +1,58 @@
 </div>
 <!-- container -->
 <footer class="footer">
+	<section>
+		<?php 
+			$get_parent_cats = array(
+				'parent' => '0' //get top level categories only
+			); 
+
+			$all_categories = get_categories( $get_parent_cats );//get parent categories 
+
+			foreach( $all_categories as $single_category ){
+				//for each category, get the ID
+				$catID = $single_category->cat_ID;
+
+				echo '<li><a href=" ' . get_category_link( $catID ) . ' ">' . $single_category->name . '</a>'; //category name & link
+					echo '<ul class="post-title">';
+
+				$query = new WP_Query( array( 'cat'=> $catID, 'posts_per_page'=>-1 ) );
+				while( $query->have_posts() ):$query->the_post();
+					echo '<li><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>';
+				endwhile;
+				wp_reset_postdata();
+
+				echo '</ul>';
+				$get_children_cats = array(
+					'child_of' => $catID //get children of this parent using the catID variable from earlier
+				);
+
+				$child_cats = get_categories( $get_children_cats );//get children of parent category
+				echo '<ul class="children">';
+					foreach( $child_cats as $child_cat ){
+						//for each child category, get the ID
+						$childID = $child_cat->cat_ID;
+
+						//for each child category, give us the link and name
+						echo '<a href=" ' . get_category_link( $childID ) . ' ">' . $child_cat->name . '</a>';
+
+							echo '<ul class="post-title">';
+
+						$query = new WP_Query( array( 'cat'=> $childID, 'posts_per_page'=>-1 ) );
+						while( $query->have_posts() ):$query->the_post();
+							echo '<li><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>';
+						endwhile;
+						wp_reset_postdata();
+
+						echo '</ul>';
+
+					}
+				echo '</ul></li>';
+			} //end of categories logic ?>
+		</ul>
+	</section>
 	<!-- footer-widgets -->
-	<div class="footer-widgets clearfix">
+	<section class="footer-widgets">
 
 		<?php if (is_active_sidebar('footer1')) : ?>
 
@@ -36,13 +86,15 @@
 
 		<?php endif; ?>
 
-	</div>
+	</section>
 	<!-- /footer-widgets -->
 
-	<p>
-		<em><?php bloginfo('name'); ?></em> - &copy; <?php echo date('Y');?></p>
-		<?php echo wordpressify_output_socials(); ?>
-	</p>
+	<section class="riangle">
+		<p>
+			<span><?php bloginfo('name'); ?></span> - &copy; <?php echo date('Y');?></p>
+		</p>
+		<img src="<?php echo get_template_directory_uri(); ?>/img/riangle.svg" alt="Riangle 🔺">
+	</section>
 
 </footer>
 <?php wp_footer(); ?>
