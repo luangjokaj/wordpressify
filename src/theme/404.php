a072fd9473718b927984dfa3531dc9ed22c54eb7
Luan Gjokaj <luan@deep-impact.ch>
2017-12-03 02:27:50 +0100

    Cleanup.

--- src/theme/404.php
+++ src/theme/404.php
@@ -4,7 +4,6 @@
 	<div id="primary" class="not-found">
 		<section class="error-404 not-found">
 			<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'lk-wordpress-theme' ); ?></h1>
-			<img src="<?php echo get_template_directory_uri(); ?>/img/sad.svg" alt="Nothing Found 😔" class="sad-icon">
 			<div class="page-content">
 				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
 
