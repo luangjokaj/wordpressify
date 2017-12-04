79d14fd8eb94667bd252b5d859f00dadf136be94
Luan Gjokaj <luan@deep-impact.ch>
2017-12-04 01:04:05 +0100

    Formatting.

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -1,5 +1,4 @@
 <?php
-
 function wordpressify_resources() {
 	wp_enqueue_style('style', get_stylesheet_uri());
 	wp_enqueue_script('header_js', get_template_directory_uri() . '/js/header-bundle.js', null, 1.0, false);
