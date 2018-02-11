ef626b27ce91ff2288ced24a72517fb232dc16f0
Luan Gjokaj <luan@riangle.com>
2018-02-11 16:57:43 +0100

    Update.

--- src/theme/front-page.php
+++ src/theme/front-page.php
@@ -4,7 +4,7 @@
 		<h1 data-aos="slow-title">A build system designed to automate your WordPress development workflow.</h1>
 		<div class="codecolorer-container javascript solarized-dark home" data-aos="slow-up">
 			<img src="<?php echo get_template_directory_uri(); ?>/img/browser.svg" alt="Browser 🌍" class="mockup">
-			<code class="javascript codecolorer">
+			<div class="javascript codecolorer">
 				<span class="co1">// <em>Step 1</em>: Clone repository from GitHub *</span><br>
 				<i>$</i> git clone https<span class="sy0">:</span>//github.com/luangjokaj/wordpressify && cd wordpressify<br>
 				<br>
@@ -23,7 +23,7 @@
 				<img draggable="false" class="emoji" alt="🚀" src="https://s.w.org/images/core/emoji/2.3/svg/1f680.svg"><br>
 				<br>
 				<span class="co1">// * Step 1,2 and 3 are part of the installation process.</span>
-			</code>
+			</div>
 		</div>
 	</main>
 </section>
