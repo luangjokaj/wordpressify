283732c857510ee7c7b77a5d1399abb18633f4a7
Luan Gjokaj <luan@riangle.com>
2018-11-27 21:46:20 +0100

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -155,9 +155,9 @@ gulp.task(
 			() => {
 				browserSync({
 					logPrefix: wpFy,
-					proxy: '127.0.0.1',
+					proxy: '127.0.0.1:3020',
 					host: '127.0.0.1',
-					port: '3000',
+					port: '3010',
 					open: 'external'
 				});
 			},
