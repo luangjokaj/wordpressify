23d693513b2c7e69220d2406cdbe2d1d224dc671
Luan Gjokaj <luan@riangle.com>
2018-11-27 21:21:50 +0100

    Typo

--- gulpfile.js
+++ gulpfile.js
@@ -155,7 +155,9 @@ gulp.task(
 			() => {
 				browserSync({
 					logPrefix: wpFy,
-					proxy: '127.0.0.1:3020',
+					proxy: '127.0.0.1',
+					
+					',
 					host: '127.0.0.1',
 					port: '3000',
 					open: 'external'
