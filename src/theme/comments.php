06cc28ca9afa931850e80732c01c9c8b7e611efd
Cresencio Cantu <cresencioc@smu.edu>
2019-05-17 15:38:06 -0500

    Escape the output properly

--- src/theme/comments.php
+++ src/theme/comments.php
@@ -30,17 +30,18 @@ if ( post_password_required() ) {
 			<?php
 			$asp_theme_comment_count = get_comments_number();
 			if ( '1' === $asp_theme_comment_count ) {
+				
 				printf(
 					/* translators: 1: title. */
 					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'asp-theme' ),
-					'<span>' . get_the_title() . '</span>'
+					'<span>' . esc_html( get_the_title() ) . '</span>'
 				);
 			} else {
 				printf( // WPCS: XSS OK.
 					/* translators: 1: comment count number, 2: title. */
 					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $asp_theme_comment_count, 'comments title', 'asp-theme' ) ),
-					number_format_i18n( $asp_theme_comment_count ),
-					'<span>' . get_the_title() . '</span>'
+					number_format( $asp_theme_comment_count ),
+					'<span>' . esc_html( get_the_title() ) . '</span>'
 				);
 			}
 			?>
