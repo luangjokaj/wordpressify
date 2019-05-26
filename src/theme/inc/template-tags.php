64e7c8b1c71f258069b4cb492cb26da8d3e3cbbf
Cresencio Cantu <holler@cresenc.io>
2019-05-26 08:48:11 -0500

    remove updated time from blog entries

--- src/theme/inc/template-tags.php
+++ src/theme/inc/template-tags.php
@@ -14,14 +14,12 @@ if ( ! function_exists( 'a_starting_point_posted_on' ) ) :
 	function a_starting_point_posted_on() {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
 		}
 
 		$time_string = sprintf( $time_string,
 			esc_attr( get_the_date( DATE_W3C ) ),
-			esc_html( get_the_date() ),
-			esc_attr( get_the_modified_date( DATE_W3C ) ),
-			esc_html( get_the_modified_date() )
+			esc_html( get_the_date() )
 		);
 
 		$posted_on = sprintf(
