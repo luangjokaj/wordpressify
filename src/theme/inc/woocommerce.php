6983add1a6a3dc1a275dfc44324c371cd9abed76
Cresencio Cantu <holler@cresenc.io>
2019-07-16 19:01:58 -0500

    Update woocommerce assets with correct text domain

--- src/theme/inc/woocommerce.php
+++ src/theme/inc/woocommerce.php
@@ -29,7 +29,7 @@ add_action( 'after_setup_theme', 'a_starting_point__woocommerce_setup' );
  * @return void
  */
 function a_starting_point__woocommerce_scripts() {
-	wp_enqueue_style( 'asp-theme-woocommerce-style', get_template_directory_uri() . '/woocommerce.css' );
+	wp_enqueue_style( 'a-starting-point-woocommerce-style', get_template_directory_uri() . '/woocommerce.css' );
 
 	$font_path   = WC()->plugin_url() . '/assets/fonts/';
 	$inline_font = '@font-face {
@@ -43,7 +43,7 @@ function a_starting_point__woocommerce_scripts() {
 			font-style: normal;
 		}';
 
-	wp_add_inline_style( 'asp-theme-woocommerce-style', $inline_font );
+	wp_add_inline_style( 'a-starting-point-woocommerce-style', $inline_font );
 }
 add_action( 'wp_enqueue_scripts', 'a_starting_point__woocommerce_scripts' );
 
