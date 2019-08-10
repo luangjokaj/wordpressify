4a9904f6ac9f56c3e9e18244603cf3c71548d26a
Luan Gjokaj <luan@riangle.com>
2019-08-11 01:03:59 +0200

    👌 IMPROVE: Cleanup default template

--- src/theme/content.php
+++ src/theme/content.php
@@ -1,5 +1,4 @@
-<article class="grid-item post <?php if ( has_post_thumbnail() ) {
-?>has-thumbnail <?php } ?>" data-aos="fade-up">
+<div class="item <?php if ( has_post_thumbnail() ) { ?>has-thumbnail <?php } ?>" data-aos="fade-up">
 	<!-- post-thumbnail -->
 	<div class="post-thumbnail">
 		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'small-thumbnail' ); ?></a>
@@ -8,7 +7,7 @@
 
 	<!-- inner-content -->
 	<div class="inner-content">
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<h2 class="item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<p class="post-info">
 			<?php echo get_the_excerpt(); ?>
 			<?php
@@ -16,38 +15,26 @@
 			$separator  = ', ';
 			$output     = '';
 
-			if ( $categories ) {
-				?>
+			if ( $categories ) : ?>
 				<div class="tags">
-					<?php
-					foreach ( $categories as $category ) {
-						$output .= '<a href="' . get_category_link( $category->term_id ) . '">' . $category->cat_name . '</a>' . $separator;
-					}
-					echo trim( $output, $separator );
+					<?php foreach ( $categories as $category )
+						{ $output .= '<a href="' . get_category_link( $category->term_id ) . '">' . $category->cat_name . '</a>' . $separator; }
+						echo trim( $output, $separator ); 
 					?>
 				</div>
-				<?php
-			}
-			?>
+			<?php endif; ?>
 		</p>
 
-		<?php if ( is_search() or is_archive() ) { ?>
+		<?php if ( is_search() or is_archive() ) : ?>
 			<p class="excerpt content">
 				<a href="<?php the_permalink(); ?>" class="button">Read More</a>
 			</p>
-		<?php
-} else {
-	if ( $post->post_excerpt ) {
-			?>
+		<?php endif; if ( $post->post_excerpt ) : ?>
 			<p class="excerpt">
 				<?php echo get_the_excerpt(); ?>
 				<a href="<?php the_permalink(); ?>" class="button">Read More</a>
 			</p>
-			<?php
-	} else {
-	}
-}
-		?>
+		<?php endif; ?>
 	</div>
 	<!-- /inner-content -->
-</article>
+</div>
--- tools/fresh-theme/theme/header.php
+++ tools/fresh-theme/theme/header.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110577987-1"></script>
-	<script>
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-
-	gtag('config', 'UA-110577987-1');
-	</script>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
 	<?php wp_head(); ?>
