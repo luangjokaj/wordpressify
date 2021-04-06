46f76799fbe198e81da1792e36eb0e7ed6560546
Luan Gjokaj <luan@riangle.com>
2021-04-06 22:22:47 +0200

    feat: load fonts asynchronously

--- src/theme/header.php
+++ src/theme/header.php
@@ -5,6 +5,28 @@
 	<meta name="author" content="Luan Gjokaj, and WordPressify contributors" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<?php wp_head(); ?>
+	<link
+		rel="preconnect"
+		href="https://fonts.gstatic.com"
+		crossOrigin="true"
+	/>
+	<link
+		rel="preload"
+		as="style"
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+		media="print"
+		onLoad="this.media='all'"
+	/>
+	<noscript>
+		<link
+			rel="stylesheet"
+			href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+		/>
+	</noscript>
 </head>
 
 <body <?php body_class(); ?>>
