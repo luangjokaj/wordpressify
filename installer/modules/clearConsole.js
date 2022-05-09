a254485d9d84b21f7ad1ba7faca8a2a930de45df
Luan Gjokaj <luan@riangle.com>
2022-05-09 21:31:28 +0200

    feat: add installer

---
+++ installer/modules/clearConsole.js
@@ -0,0 +1,13 @@
+/**
+ * Cross platform clear console.
+ *
+ * Support for win32 and others.
+ */
+
+const clearConsole = () => {
+	process.stdout.write(
+		'win32' === process.platform ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H'
+	);
+};
+
+export { clearConsole };
